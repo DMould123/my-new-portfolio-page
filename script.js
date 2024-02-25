4d96f1b384bdb811741c51b28e176479ecca89d1
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-02-25 14:16:36 +0100

    Updated script.js to ensure better readibility and maintainability

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
 const header = document.querySelector('header')
+const menu = document.querySelector('#menu-icon')
+const navlist = document.querySelector('.navlist')
 
-window.addEventListener('scroll', function () {
+// Function to toggle sticky class on header based on scroll position
+const toggleStickyHeader = () => {
   header.classList.toggle('sticky', window.scrollY > 0)
-})
+}
 
-let menu = document.querySelector('#menu-icon')
-let navlist = document.querySelector('.navlist')
-
-menu.onclick = () => {
+// Function to toggle menu icon and navlist
+const toggleMenu = () => {
   menu.classList.toggle('bx-x')
   navlist.classList.toggle('active')
 }
 
-window.onscroll = () => {
-  menu.classList.remove('bx-x')
-  navlist.classList.remove('active')
-}
+// Event listener for scrolling
+window.addEventListener('scroll', () => {
+  toggleStickyHeader()
+  // Close menu on scroll
+  toggleMenu(false)
+})
 
+// Event listener for menu icon click
+menu.addEventListener('click', () => {
+  toggleMenu()
+})
+
+// Initialize ScrollReveal
 const sr = ScrollReveal({
   distance: '45px',
   duration: 2700,
   reset: true
 })
 
-sr.reveal('.home-text', { delay: 350, origin: 'left' })
-sr.reveal('.home-img', { delay: 350, origin: 'right' })
-
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact', {
-  delay: 200,
-  origin: 'bottom'
+// Reveal animations
+const revealElements = [
+  '.home-text',
+  '.home-img',
+  '.sub-service',
+  '.about',
+  '.portfolio',
+  '.service',
+  '.cta',
+  '.contact'
+]
+revealElements.forEach((element, index) => {
+  sr.reveal(element, {
+    delay: 200 + index * 150,
+    origin: index < 2 ? 'left' : 'bottom'
+  })
 })
